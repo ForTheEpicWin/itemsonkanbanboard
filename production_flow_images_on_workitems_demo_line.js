95754f8092927fb5d9c0999e2c393f0d1b46a4ce
Feiko <ForTheEpicWin@users.noreply.github.com>
2023-08-24 12:00:08 +0200

    Update production_flow_images_on_workitems_demo_line.js

--- production_flow_images_on_workitems_demo_line.js
+++ production_flow_images_on_workitems_demo_line.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Images on DEVOPS - DEMO PRODUCTION FLOW
 // @namespace    https://makeworkflow.de
-// @version      1.2
+// @version      1.3
 // @description  Inserts an image from a specified Workitemfield into a the specific workitem on a the kanban board page from production.flow
 // @match        https://dev.azure.com/MWF-Demo-Line/production.flow
+// @match        https://dev.azure.com/MWF-Demo-Line/production.flow/_boards/board/t/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @copyright    MAKE WORK FLOW GmbH
