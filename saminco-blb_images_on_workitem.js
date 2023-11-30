fb0debd10288908f1fe2d968236feda901bb2407
Feiko <ForTheEpicWin@users.noreply.github.com>
2023-11-30 16:00:17 +0100

    updated revision

--- saminco-blb_images_on_workitem.js
+++ saminco-blb_images_on_workitem.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Images on DEVOPS - Saminco - BLB
 // @namespace    https://makeworkflow.de
-// @version      1.2
+// @version      1.2.1
 // @description  Inserts an image from a specified Workitemfield into a the specific workitem on a the kanban board page from production.flow
 // @match        https://dev.azure.com/MWF-Development-Boards/Saminco-BLB
 // @match        https://dev.azure.com/MWF-Development-Boards/Saminco-BLB/_boards/board/t/*
