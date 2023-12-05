edc3b3bf47422ce571167cdf26e58e8331acd410
Feiko <ForTheEpicWin@users.noreply.github.com>
2023-12-05 13:54:16 +0100

    Updating translation saminco-blb

--- saminco-blb_images_on_workitem.js
+++ saminco-blb_images_on_workitem.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Images on DEVOPS - Saminco - BLB
 // @namespace    https://makeworkflow.de
-// @version      1.2.6
+// @version      1.2.7
 // @description  Inserts an image from a specified Workitemfield into a the specific workitem on a the kanban board page from production.flow
 // @match        https://dev.azure.com/MWF-Development-Boards/Saminco-BLB
 // @match        https://dev.azure.com/MWF-Development-Boards/Saminco-BLB/_boards/board/t/*
@@ -30,7 +30,7 @@
 
     // WORDS TO TRANSLATE 
     const translationDictionary = {
-        "CUSTOM07": "COLORWAY",
+        "CUSTOM07": "颜色||COLORWAY",
         "CUSTOM08": "序列号||S/N NUM",
         "ORDER QTY": "订单数量||ORDER QTY",
         "SIZE_": "码数||SIZE ",
