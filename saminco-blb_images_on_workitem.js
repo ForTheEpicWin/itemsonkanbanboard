aa69b80fa7fc7eb7744087e193107485fa922ea4
Feiko <ForTheEpicWin@users.noreply.github.com>
2023-12-13 22:38:47 +0100

    added depencies that are reachable from mainland

--- saminco-blb_images_on_workitem.js
+++ saminco-blb_images_on_workitem.js
@@ -1,86 +1,20 @@
 // ==UserScript==
 // @name         Images on DEVOPS - Saminco - BLB
 // @namespace    https://makeworkflow.de
-// @version      1.3.3
+// @version      1.3.4
 // @description  Inserts an image from a specified Workitemfield into a the specific workitem on a the kanban board page from production.flow
 // @match        https://dev.azure.com/MWF-Development-Boards/Saminco-BLB
 // @match        https://dev.azure.com/MWF-Development-Boards/Saminco-BLB/_boards/board/t/*
 // @match        https://dev.azure.com/Saminco/B.L.B%20FOOTWEAR%20PRODUCTION
 // @match        https://dev.azure.com/Saminco/B.L.B%20FOOTWEAR%20PRODUCTION/_boards/board/t/*
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
+// @require      https://gist.gitmirror.com/raw/2625891/waitForKeyElements.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @copyright    MAKE WORK FLOW GmbH
 // @author       Feiko Bronsveld
 // @unwrap
 // ==/UserScript==
-
-function waitForKeyElements(
-  selectorTxt /* Required: The jQuery selector string that
-                      specifies the desired element(s).
-                  */,
-  actionFunction /* Required: The code to run when elements are
-                      found. It is passed a jNode to the matched
-                      element.
-                  */,
-  bWaitOnce /* Optional: If false, will continue to scan for
-                      new elements even after the first match is
-                      found.
-                  */,
-  iframeSelector /* Optional: If set, identifies the iframe to
-                      search.
-                  */
-) {
-  var targetNodes, btargetsFound;
-
-  if (typeof iframeSelector == "undefined") targetNodes = $(selectorTxt);
-  else targetNodes = $(iframeSelector).contents().find(selectorTxt);
-
-  if (targetNodes && targetNodes.length > 0) {
-    btargetsFound = true;
-    /*--- Found target node(s).  Go through each and act if they
-          are new.
-      */
-    targetNodes.each(function () {
-      var jThis = $(this);
-      var alreadyFound = jThis.data("alreadyFound") || false;
-
-      if (!alreadyFound) {
-        //--- Call the payload function.
-        var cancelFound = actionFunction(jThis);
-        if (cancelFound) btargetsFound = false;
-        else jThis.data("alreadyFound", true);
-      }
-    });
-  } else {
-    btargetsFound = false;
-  }
-
-  //--- Get the timer-control variable for this selector.
-  var controlObj = waitForKeyElements.controlObj || {};
-  var controlKey = selectorTxt.replace(/[^\w]/g, "_");
-  var timeControl = controlObj[controlKey];
-
-  //--- Now set or clear the timer as appropriate.
-  if (btargetsFound && bWaitOnce && timeControl) {
-    //--- The only condition where we need to clear the timer.
-    clearInterval(timeControl);
-    delete controlObj[controlKey];
-  } else {
-    //--- Set a timer, if needed.
-    if (!timeControl) {
-      timeControl = setInterval(function () {
-        waitForKeyElements(
-          selectorTxt,
-          actionFunction,
-          bWaitOnce,
-          iframeSelector
-        );
-      }, 300);
-      controlObj[controlKey] = timeControl;
-    }
-  }
-  waitForKeyElements.controlObj = controlObj;
-}
 
 (function() {
     // ON IMAGE FIELD FOUND
